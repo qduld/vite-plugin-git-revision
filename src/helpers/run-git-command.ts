48f6a9d765d5d582d786392e98865386e160fc23
franklin_zelo <490334843@qq.cpm>
2021-04-21 14:56:48 +0800

    init: project init
    1. get git version

---
+++ src/helpers/run-git-command.ts
@@ -0,0 +1,28 @@
+var exec = require('child_process').exec
+var execSync = require('child_process').execSync
+// import { exec,execSync } from 'child_process';
+import path from 'path';
+import { removeEmptyLines } from './remove-empty-lines';
+
+export function runGitCommand(gitWorkTree:any, command:any, callback:Function) {
+  var gitCommand = gitWorkTree
+    ? [
+      'git',
+      '--git-dir=' + path.join(gitWorkTree, '.git'),
+      '--work-tree=' + gitWorkTree,
+      command
+    ].join(' ')
+    : [
+      'git',
+      command
+    ].join(' ')
+
+  if (callback) {
+    exec(gitCommand, function (err:any, stdout:any) {
+      if (err) { return callback(err) }
+      callback(null, removeEmptyLines(stdout))
+    })
+  } else {
+    return removeEmptyLines('' + execSync(gitCommand))
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { Plugin } from 'vite'
+import { runGitCommand } from './helpers/run-git-command'
+
+const COMMITHASH_COMMAND = 'rev-parse HEAD'
+const VERSION_COMMAND = 'describe --always'
+const BRANCH_COMMAND = 'rev-parse --abbrev-ref HEAD'
+
+const defaultOpt = {
+    lightweightTags:false,
+    branch:false,
+    commithashCommand:COMMITHASH_COMMAND,
+    versionCommand:VERSION_COMMAND,
+    branchCommand:BRANCH_COMMAND,
+}
+
+export interface ViteGitRevisionPlugin {
+    // git work tree
+    gitWorkTree?: any;
+    // lightweight tags support.
+    lightweightTags?: boolean;
+    // branch name support
+    branch?: boolean;
+    //change the default git command used to read the value of COMMITHASH
+    commithashCommand?: string;
+    //change the default git command used to read the value of VERSION
+    versionCommand?: string;
+    //change the default git command used to read the value of BRANCH
+    branchCommand?: string;
+}
+
+
+export default (options: ViteGitRevisionPlugin): Plugin => {
+    options = Object.assign(defaultOpt,options)
+    if (options.versionCommand && options.lightweightTags) {
+        throw new Error('lightweightTags can\'t be used together versionCommand')
+    }
+  
+    return {
+      name: 'vite:git-revision',
+      config(resolvedConfig) {
+        console.log(123)
+        console.log(runGitCommand(options.gitWorkTree,options.commithashCommand,()=>{}))
+      },
+    };
+  };
