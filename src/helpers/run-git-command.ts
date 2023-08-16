d13b6eea258bd2d1bbfe88c42db64385c43a46f1
Jin <wenjin.chan@ncs.com.sg>
2023-08-16 14:01:01 +0800

    fix: GitRevision is not a function when using type module

--- src/helpers/run-git-command.ts
+++ src/helpers/run-git-command.ts
@@ -1,10 +1,14 @@
-var exec = require('child_process').exec
-var execSync = require('child_process').execSync
-// import { exec,execSync } from 'child_process';
-import path from 'path';
-import { removeEmptyLines } from './remove-empty-lines';
+import * as child from "child_process";
+import * as path from "path";
+import { removeEmptyLines } from "./remove-empty-lines";
 
-export function runGitCommand(gitWorkTree:any, command:any, callback?:Function) {
+const { exec, execSync } = child;
+
+export function runGitCommand(
+  gitWorkTree: any,
+  command: any,
+  callback?: Function
+) {
   var gitCommand = gitWorkTree
     ? [
       'git',
